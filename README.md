8b00de44e78c5deabae2f3aaf5e3834f7a4d3985
AdamWynne <awynne@gmail.com>
2016-10-06 23:49:54 -0400

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+parallelize listing: http://docs.spring.io/spring/docs/current/spring-framework-reference/html/scheduling.html
--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,13 +1,27 @@
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.Bucket
 import com.amazonaws.services.s3.AmazonS3Client
+import java.util.concurrent.*
+import org.apache.commons.cli.*
 
 AmazonS3 s3 = new AmazonS3Client()
 List<Bucket> buckets = s3.listBuckets()
 
-buckets.each {
-	println "buck: ${it}"
-}
+def meth = System.getProperty("method")
+if(meth) {
+    println("running: ${meth}")
+    def run = {bucket -> println "buck: ${bucket}"}
+    def pool = Executors.newFixedThreadPool(4)
 
+    List<Future> futures = buckets.collect { bucket ->
+        pool.submit({-> run bucket } as Callable);
+    }
+    
+    futures.each{it.get()}
+    pool.shutdown()
+}
+else {
+    print "whoops"
+}
 
 
